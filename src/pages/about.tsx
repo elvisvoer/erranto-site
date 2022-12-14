cb8a4bc0f1aa3b8fcabab06df203f1ea3d5d97c7
Elvis <elvis.adomnica@gmail.com>
2022-12-14 23:45:34 +0100

    Add heading prompt to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,10 +4,15 @@ import { jsx } from "theme-ui";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 import Elvis from "../components/elvis";
+import Prompt from "../components/prompt";
 
 const AboutPage = () => {
   return (
     <Layout>
+      <div>
+        <Prompt />
+        cat about.mdx | mdx-viewer
+      </div>
       <div>This is a simple about page.</div>
       <Elvis />
     </Layout>
