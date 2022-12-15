5d18bc163f55e2829103ea67d4a1d29a1729251d
Elvis <elvis.adomnica@gmail.com>
2022-12-15 19:08:09 +0100

    Yet another responsive adjustment and colors for prompt

--- src/components/prompt.tsx
+++ src/components/prompt.tsx
@@ -1,3 +1,10 @@
 import React from "react";
+/** @jsx jsx */
+import { jsx } from "theme-ui";
 
-export default () => <span>[elvis@erranto.com ~]$&nbsp;</span>;
+export default () => (
+  <span>
+    [<span sx={{ color: "gray" }}>elvis@erranto.com</span>{" "}
+    <span sx={{ color: "secondary" }}>~</span>]$&nbsp;
+  </span>
+);
