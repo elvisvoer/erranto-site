53eca5fea31077380d5f2130c2b75619165bd502
Elvis <elvis.adomnica@gmail.com>
2022-12-30 23:54:24 +0100

    Fix 404 error on /blog url

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,68 @@
+import * as React from "react";
+import { Link, graphql } from "gatsby";
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import Layout from "../../components/layout";
+import Seo from "../../components/seo";
+import Prompt from "../../components/prompt";
+
+const IndexPage = ({ data }: any) => {
+  const totalSize = data.allMdx.nodes.reduce((acc: number, node: any) => {
+    return acc + node.parent.size;
+  }, 0);
+
+  return (
+    <Layout>
+      <div>
+        <Prompt />
+        ls -l blog/
+      </div>
+      <div>total {totalSize}</div>
+      <table sx={{ width: "100%" }}>
+        <tbody>
+          {data.allMdx.nodes
+            .filter((n: any) => n)
+            .map(
+              (node: any) =>
+                node.frontmatter.slug && (
+                  <tr key={node.id}>
+                    <td>-rw-rw-r-- 1 elvis elvis</td>
+                    <td>{node.parent.size}</td>
+                    <td>{node.frontmatter.date}</td>
+                    <td>
+                      <Link to={`/blog/${node.frontmatter.slug}`}>
+                        {node.frontmatter.slug}.mdx
+                      </Link>
+                    </td>
+                  </tr>
+                )
+            )}
+        </tbody>
+      </table>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query {
+    allMdx(sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        frontmatter {
+          slug
+          date(formatString: "MMMM D, YYYY")
+        }
+        id
+        parent {
+          id
+          ... on File {
+            size
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const Head = () => <Seo title="Blog" />;
+
+export default IndexPage;
