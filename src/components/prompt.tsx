94bf9ce7b36120d789c147dc4a4583f31bc32b02
Elvis <elvis.adomnica@gmail.com>
2022-12-31 13:25:50 +0100

    Remove extra space from prompt

--- src/components/prompt.tsx
+++ src/components/prompt.tsx
@@ -5,6 +5,6 @@ import { jsx } from "theme-ui";
 export default () => (
   <span>
     <span sx={{ color: "gray" }}>elvis@erranto.com</span>{" "}
-    <span sx={{ color: "secondary" }}>~</span>$&nbsp;
+    <span sx={{ color: "secondary" }}>~</span>$
   </span>
 );
