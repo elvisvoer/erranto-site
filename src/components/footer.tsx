eeb5b8433f45a5b9b86952f5d40ed6c2af16c5ee
Elvis <elvis.adomnica@gmail.com>
2023-01-03 16:30:04 +0100

    Switch to 17px

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,7 +9,7 @@ export default () => (
     <div>
       <Prompt /> echo $COPYRIGHT
     </div>
-    <pre sx={{ fontSize: ["2.15vw", ".7rem", ".95rem"] }}>
+    <pre sx={{ fontSize: ["2.15vw", ".7rem", "1.07rem"] }}>
       {`*************************************************************************
 *                                                                       *
 *                           COPYRIGHT NOTICE                            *
