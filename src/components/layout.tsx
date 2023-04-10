710a670e87158aab5d168c6c518867a276a5e3c9
Elvis <elvis.adomnica@gmail.com>
2023-04-10 12:00:37 +0200

    Add basic terminal component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,25 +1,14 @@
 import * as React from "react";
 import { Link } from "gatsby";
 /** @jsx jsx */
-import {
-  jsx,
-  ThemeProvider,
-  InitializeColorMode,
-  useColorMode,
-} from "theme-ui";
-import { Themed } from "@theme-ui/mdx";
-import { tosh } from "@theme-ui/presets";
+import { jsx } from "theme-ui";
 import Logo from "./logo";
 import Prompt from "./prompt";
 import Footer from "./footer";
+import { ThemeProvider, ThemeChanger } from "./theme";
 
 type LayoutProps = {
   children: React.ReactNode | React.ReactNode[];
-};
-
-const themeLink = {
-  textDecoration: "underline",
-  cursor: "pointer",
 };
 
 const navLinks = {
@@ -33,67 +22,34 @@ const navLinkItem = {
   paddingRight: "2rem",
 };
 
-const ThemeChanger = () => {
-  const [colorMode, setColorMode] = useColorMode();
-
-  const setDarkMode = (isDark: boolean) =>
-    setColorMode(isDark ? "dark" : "default");
-
-  return (
-    <>
-      <div>
-        Color mode: {colorMode && colorMode.replace("default", "light")}
-      </div>
-      <div>
-        Available color modes:{" "}
-        <span style={themeLink} onClick={() => setDarkMode(false)}>
-          light
-        </span>
-        ,{" "}
-        <span style={themeLink} onClick={() => setDarkMode(true)}>
-          dark
-        </span>
-      </div>
-    </>
-  );
-};
-
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <ThemeProvider theme={tosh}>
-      <Themed.root
-        sx={{
-          padding: 2,
-          maxWidth: 760,
-          display: "flex",
-          flexDirection: "column",
-          minHeight: "100vh",
-        }}
-      >
-        <div sx={{ marginBottom: 3 }}>
-          <InitializeColorMode />
-          <Logo />
-          <ThemeChanger />
-        </div>
-        <nav sx={{ marginBottom: 3 }}>
-          <span>
-            <Prompt /> ls
-          </span>
-          <ul style={navLinks}>
-            <li style={navLinkItem}>
-              <Link to="/">blog</Link>
-            </li>
-            <li style={navLinkItem}>
-              <Link to="/about">about.mdx</Link>
-            </li>
-            <li style={navLinkItem}>
-              <Link to="/rss.xml">rss.xml</Link>
-            </li>
-          </ul>
-        </nav>
-        <main sx={{ flex: 1 }}>{children}</main>
-        <Footer />
-      </Themed.root>
+    <ThemeProvider>
+      <div sx={{ marginBottom: 3 }}>
+        <Logo />
+        <ThemeChanger />
+      </div>
+      <nav sx={{ marginBottom: 3 }}>
+        <span>
+          <Prompt /> ls
+        </span>
+        <ul style={navLinks}>
+          <li style={navLinkItem}>
+            <Link to="/">blog</Link>
+          </li>
+          <li style={navLinkItem}>
+            <Link to="/tty">terminal</Link>
+          </li>
+          <li style={navLinkItem}>
+            <Link to="/about">about.mdx</Link>
+          </li>
+          <li style={navLinkItem}>
+            <Link to="/rss.xml">rss.xml</Link>
+          </li>
+        </ul>
+      </nav>
+      <main sx={{ flex: 1 }}>{children}</main>
+      <Footer />
     </ThemeProvider>
   );
 };
