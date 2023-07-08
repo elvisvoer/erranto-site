c866d478a28caf24a3326f9bd0b0022a9f46d8f3
Elvis <elvis.adomnica@gmail.com>
2023-07-08 13:48:37 +0200

    Move site to astro

--- src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import Seo from "../components/seo";
-import { IndexPage } from "./blog";
-
-export default IndexPage;
-
-export const Head = () => <Seo title="Blog" />;
