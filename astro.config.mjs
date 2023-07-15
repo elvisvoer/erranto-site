dafecb69657d44845a9d6c672eb537b9cff09da6
Elvis <elvis.adomnica@gmail.com>
2023-07-15 18:17:01 +0200

    Fix astro site config to fix the RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,11 @@ import tailwind from "@astrojs/tailwind";
 
 import svelte from "@astrojs/svelte";
 
-const isDev = import.meta.env.MODE === "development";
 const DEV_PORT = 2121;
 
 // https://astro.build/config
 export default defineConfig({
-  site: isDev ? `http://localhost:${DEV_PORT}` : "https://erranto.com",
+  site: "https://erranto.com",
 
   server: {
     /* Dev. server only */
