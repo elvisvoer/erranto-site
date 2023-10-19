224d9f7978787d90111eabb214c5e8442b48ddf7
Elvis Adomnica <e.adomnica@sprinteins.com>
2023-10-19 12:06:12 +0200

    Update rss feed author end description

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,6 +2,7 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import MarkdownIt from "markdown-it";
 import sanitizeHtml from "sanitize-html";
+import pkg from "../../package.json" assert { type: "json" };
 const parser = new MarkdownIt();
 
 export async function get(context) {
@@ -9,14 +10,14 @@ export async function get(context) {
 
   return rss({
     title: "Erranto RSS Feed",
-    description: "Erranto Blog RSS Feed",
+    description: pkg.description,
     site: context.site,
     items: blog.map((post) => ({
       title: post.data.title,
       pubDate: post.data.created,
       link: `/blog/${post.slug}`,
       content: sanitizeHtml(parser.render(post.body)),
-      author: "Elvis Adomnica",
+      author: pkg.author,
       categories: post.data.tags,
     })),
   });
