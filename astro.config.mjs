8ddd9e8c7c85581f6427e0d69edc8efd02b94d65
Elvis <elvis.adomnica@gmail.com>
2024-01-09 12:39:35 +0100

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,16 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
-
+import sitemap from "@astrojs/sitemap";
 const DEV_PORT = 2121;
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://erranto.com",
-
   server: {
     /* Dev. server only */
     port: DEV_PORT,
   },
-
-  integrations: [mdx(), tailwind(), svelte()],
+  integrations: [mdx(), tailwind(), svelte(), sitemap()],
 });
