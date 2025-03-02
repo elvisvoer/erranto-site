33999bdbcca4bb5ee7b9b223b9c7694cd44bec68
Elvis <elvis.adomnica@gmail.com>
2025-03-02 17:27:40 +0100

    Add support for executing async functions

--- src/lib/javascript.ts
+++ src/lib/javascript.ts
@@ -2,9 +2,12 @@ const defaultContext: any = {
   console,
 };
 
+const AsyncFunction = async function () {}.constructor;
+
 export function evalSafe(code: string, ctx = defaultContext) {
   try {
-    new Function(...Object.keys(ctx), code).bind(ctx)(...Object.values(ctx));
+    // @ts-expect-error
+    new AsyncFunction(...Object.keys(ctx), code).bind(ctx)(...Object.values(ctx));
   } catch (err) {
     ctx.console.error(err);
   }
