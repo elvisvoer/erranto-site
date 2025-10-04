3447f4d9ce6b5ad6ba8ee29e9870494e19f6b87c
Elvis <elvis.adomnica@gmail.com>
2025-10-04 11:52:49 +0200

    Add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,19 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import sitemap from "@astrojs/sitemap";
 import db from "@astrojs/db";
+import vercel from "@astrojs/vercel";
 const DEV_PORT = 2121;
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://erranto.com",
+
   server: {
     /* Dev. server only */
     port: DEV_PORT,
   },
+
   integrations: [mdx(), tailwind(), svelte(), sitemap(), db()],
   trailingSlash: "never",
+  adapter: vercel(),
 });
