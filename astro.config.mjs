5bce43c4a2175ca94872c6bcd4176d7b4d19e92a
Elvis <elvis.adomnica@gmail.com>
2025-10-04 12:55:47 +0200

    Add sign in

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import sitemap from "@astrojs/sitemap";
 // import db from "@astrojs/db";
 import vercel from "@astrojs/vercel";
+import clerk from '@clerk/astro'
 const DEV_PORT = 2121;
 
 // https://astro.build/config
@@ -16,7 +17,13 @@ export default defineConfig({
     port: DEV_PORT,
   },
 
-  integrations: [mdx(), tailwind(), svelte(), sitemap()],
+  integrations: [mdx(), tailwind(), svelte(), sitemap(), clerk()],
   trailingSlash: "never",
   adapter: vercel(),
+  env: {
+    schema: {
+      PUBLIC_CLERK_PUBLISHABLE_KEY: envField.string({ context: "server", access: "public" }),
+      CLERK_SECRET_KEY: envField.string({ context: "server", access: "secret" }),
+    }
+  }
 });
