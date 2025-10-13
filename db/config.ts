b5d892b715c892172bd527dfb32d0b07d6c1707c
Elvis <elvis.adomnica@gmail.com>
2025-10-13 09:22:35 +0200

    Edit player info

--- db/config.ts
+++ db/config.ts
@@ -1,15 +1,46 @@
-import { defineDb, defineTable, column } from 'astro:db';
+import { defineDb, defineTable, column } from "astro:db";
 
 const Player = defineTable({
   columns: {
     id: column.text({ primaryKey: true }),
     name: column.text(),
+    handicap: column.number({ default: 3 }),
+    tier: column.number({ default: 8 }),
     active: column.boolean(),
     admin: column.boolean(),
-  }
-})
+  },
+});
+
+const Match = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    type: column.text({
+      enum: ["8-ball", "9-ball", "10-ball"],
+      default: "9-ball",
+    }),
+    raceTo: column.number({ default: 9 }),
+    status: column.text({
+      enum: ["pending", "active", "finished"],
+      default: "pending",
+    }),
+  },
+});
+
+const MatchStats = defineTable({
+  columns: {
+    matchId: column.text(),
+    playerId: column.text(),
+    framesWon: column.number()
+  },
+  foreignKeys: [
+    {
+      columns: ["matchId", "playerId"],
+      references: () => [Match.columns.id, Player.columns.id],
+    },
+  ],
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {Player}
+  tables: { Player },
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,3 +1,78 @@
-import { db, User } from "astro:db";
+import { db, Player } from "astro:db";
 
-export default async function () {}
+export default async function () {
+  await db.insert(Player).values([
+    {
+      id: "1",
+      name: "Claus Julius",
+      tier: 1,
+      handicap: 2,
+      active: true,
+      admin: false,
+    },
+    {
+      id: "2",
+      name: "Martin Hermansen",
+      tier: 2,
+      handicap: 1,
+      active: true,
+      admin: false,
+    },
+    {
+      id: "3",
+      name: "Claus (Godzilla)",
+      tier: 2,
+      handicap: 2,
+      active: true,
+      admin: false,
+    },
+    {
+      id: "4",
+      name: "Troels (Tornado)",
+      tier: 3,
+      handicap: 3,
+      active: true,
+      admin: false,
+    },
+    {
+      id: "5",
+      name: "Naim",
+      tier: 3,
+      handicap: 3,
+      active: true,
+      admin: false,
+    },
+    {
+      id: "6",
+      name: "Taha",
+      tier: 3,
+      handicap: 3,
+      active: true,
+      admin: false,
+    },
+    {
+      id: "7",
+      name: "Seb",
+      tier: 4,
+      handicap: 1,
+      active: true,
+      admin: false,
+    },
+    {
+      id: "8",
+      name: "Steve",
+      tier: 4,
+      handicap: 1,
+      active: true,
+      admin: false,
+    },
+    {
+      id: "9",
+      name: "Chak",
+      tier: 4,
+      handicap: 3,
+      active: true,
+      admin: false,
+    },
+  ]);
+}
